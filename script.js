269ede66c19ae3a6726542624d88ea84cc3aec3c
siddht1 <rt1yrru+github@gmail.com>
2023-01-25 13:45:54 +0530

    error handling added
    Added error handling

--- script.js
+++ script.js
@@ -5,12 +5,21 @@ updateDataAndDOM();
 
 function updateDataAndDOM() {
   fetch('https://siddht1.github.io/train_view/1077011.json')
-    .then(response => response.json())
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
     .then(fetchedData => {
-      if (JSON.stringify(data) !== JSON.stringify(fetchedData)) {
+      if (!data || JSON.stringify(data.status) !== JSON.stringify(fetchedData.status)) {
         data = fetchedData;
         updateDOM(data);
       }
+      setTimeout(updateDataAndDOM, 5000);
+    })
+    .catch(error => {
+      console.error(error);
       setTimeout(updateDataAndDOM, 5000);
     });
 }
