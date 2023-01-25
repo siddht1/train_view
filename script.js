0cdfce51f5349d370c7f300bd757789325d524c6
siddht1 <rt1yrru+github@gmail.com>
2023-01-25 12:01:06 +0530

    made changes
    changes were made

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 const container = document.querySelector('.status');
 
-fetch('1077011.json')
+fetch('https://siddht1.github.io/train_view/1077011.json')
   .then(response => response.json())
   .then(data => {
     data.status.forEach(status => {
       const div = document.createElement('div');
-      div.classList.add(`status-${status.status}`);
+      div.classList.add(`status-${status.color}`);
       div.innerHTML = `
         <span class="server-status" type="${status.status}"></span>
         <span> ${status.name} | ${status.time}</span>
       `;
       container.appendChild(div);
+      console.log(div);
     });
   });
