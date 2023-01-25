9b6dab93f4914e05aff205e67b7f03507f268ba3
siddht1 <rt1yrru+github@gmail.com>
2023-01-25 13:37:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
-const container = document.querySelector('.status');
+let data;
 
-fetch('https://siddht1.github.io/train_view/1077011.json')
-  .then(response => response.json())
-  .then(data => {
-    data.status.forEach(status => {
-      const div = document.createElement('div');
-      div.classList.add(`status-${status.color}`);
-      div.innerHTML = `
+// Get data and update DOM initially
+updateDataAndDOM();
+
+function updateDataAndDOM() {
+  fetch('https://siddht1.github.io/train_view/1077011.json')
+    .then(response => response.json())
+    .then(fetchedData => {
+      if (JSON.stringify(data) !== JSON.stringify(fetchedData)) {
+        data = fetchedData;
+        updateDOM(data);
+      }
+      setTimeout(updateDataAndDOM, 5000);
+    });
+}
+
+function updateDOM(data) {
+  document.title = 'TRAIN STATUS|| 1077011';
+  const container = document.querySelector('.status');
+
+  let elementsString = '';
+  data.status.forEach(status => {
+    elementsString += `
+      <div class="status-${status.color}">
         <span class="server-status" type="${status.status}"></span>
         <span> ${status.name} | ${status.time}</span>
-      `;
-      container.appendChild(div);
-      console.log(div);
-    });
+      </div>
+    `;
   });
+  container.innerHTML = elementsString;
+}
