b2809535cc085916f4dc4153c23b91e375f56181
siddht1 <rt1yrru+github@gmail.com>
2023-02-16 17:27:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function updateDataAndDOM(url) {
   fetch(url)
     .then(response => {
       if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
+        throw new Error(`HTTP error! status: ${response}`);
       }
       return response.json();
     })
