3130d6f40b05dc9b9a99e5bd3c948db64dbcd78c
siddht1 <rt1yrru+github@gmail.com>
2023-02-16 22:30:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,72 @@
-let data;
+// Module for getting data and updating DOM
+const DataAndDom = (() => {
+  let data;
+  const url = 'https://train-api-git-main-siddht1.vercel.app/api/v1/trains';
 
-const updateDataAndDOM = (url) => {
-  fetch(url)
-    .then(response => {
+  const getData = async () => {
+    try {
+      const response = await fetch(url);
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
-      return response.json();
-    })
-    .then(fetchedData => {
-      if (!data || JSON.stringify(data) !== JSON.stringify(fetchedData)) 
-      {
+      const fetchedData = await response.json();
+      if (!data || JSON.stringify(data) !== JSON.stringify(fetchedData)) {
         data = fetchedData;
         console.log(data);
-        updateDOM(data);
+        Dom.update(data);
       }
-      setTimeout(() => updateDataAndDOM(url), 5000);
-    })
-    .catch(error => {
+    } catch (error) {
       console.error(error);
-      setTimeout(() => updateDataAndDOM(url), 5000);
+    }
+  };
+
+  return {
+    getData
+  };
+})();
+
+//Module for updating DOM
+const Dom = (() => {
+  const titleElement = document.querySelector('title');
+  const statusContainer = document.querySelector('.status');
+  const headerElement = document.createElement('header');
+
+  const init = () => {
+    headerElement.textContent = data.data[0].name + ' || train ' + data.data[0].id;
+    document.body.insertBefore(headerElement, document.body.firstChild);
+  };
+
+  const update = (data) => {
+    titleElement.textContent = 'TRAIN STATUS || ' + data.data[0].id;
+    let elementsString = '';
+    data.data[0].route.status.forEach(status => {
+      elementsString += `
+        <div class="status-${status.color}">
+          <span class="server-status" type="${status.status}"></span>
+          <span> ${status.name} | ${status.time}</span>
+        </div>
+      `;
     });
-};
+    statusContainer.innerHTML = elementsString;
+  };
 
-const updateDOM = ({ data }) => {
-  const { name, id, route } = data[0];
-  document.title = `TRAIN STATUS || ${id}`;
-  
-  const container = document.querySelector('.status');
-  const header = document.createElement('header');
-  header.textContent = `${name} || train ${id}`;
-  const body = document.querySelector('body');
-  body.insertBefore(header, body.firstChild);
+  return {
+    init,
+    update
+  };
+})();
 
-  const elementsString = route.status.map(status => {
-    const { color, status: type, name, time } = status;
-    return `
-      <div class="status-${color}">
-        <span class="server-status" type="${type}"></span>
-        <span> ${name} | ${time}</span>
-      </div>
-    `;
-  }).join('');
+//Initialize applications/functions
+const App = (() => {
+  const init = () => {
+    Dom.init();
+    DataAndDom.getData();
+    setInterval(DataAndDom.getData, 5000);
+  };
 
-  container.innerHTML = elementsString;
-};
+  return {
+    init
+  };
+})();
 
-const url = 'https://train-api-git-main-siddht1.vercel.app/api/v1/trains';
-updateDataAndDOM(url);
+App.init();
